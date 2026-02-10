0b3ca81f40c85c9f161dbb09a0abadd22dc237b9
seta-datnd-6862 <dat.nguyen@seta-international.vn>
2026-02-10 10:48:51 +0700

    update code

--- custom-thankyou-page.php
+++ custom-thankyou-page.php
@@ -267,7 +267,7 @@ function thank_you_inline_func() {
 
         <!-- Action Buttons -->
         <div class="tyi-action-buttons">
-            <a href="https://goodriver.online/track-order/<?php echo $order_id; ?>" class="tyi-btn tyi-btn-primary" target="_blank">
+            <a href="https://goodriver.online/guest/share/order/woocommerce/<?php echo $order_key; ?>" class="tyi-btn tyi-btn-primary" target="_blank">
                 🔍 Track My Order
             </a>
             <a href="<?php echo esc_url(wc_get_page_permalink('shop')); ?>" class="tyi-btn tyi-btn-secondary">
@@ -657,3 +657,4 @@ function get_thank_you_inline_styles() {
 		}
     ';
 }
+
